115c82114ad26c3044b108e88ef9186becf1c59b
dungatoro <71789541+dungatoro@users.noreply.github.com>
2024-06-09 12:32:56 +0100

    progress from laptop

--- marble_maze.py
+++ marble_maze.py
@@ -1,41 +1,97 @@
-import sys
-sys.path.insert(1, '../')
+import pygame
+from random import choice, randrange
 
-import lib.tilt as tilt
+RES = WIDTH, HEIGHT = 1202, 902
+TILE = 100
+cols, rows = WIDTH // TILE, HEIGHT // TILE
 
-def load_maze(width, height):
-    # create a blank grid
-    grid = {}
-    for x in range(width):
-        for y in range(height):
-            nodes = [(0, (i,j)) for i,j in [(x,y-1),(x,y+1),(x-1,y),(x+1,y)]
-                          if 0 <= i < width and 0 <= j < height ]
-            grid[(x, y)] = nodes
+class Cell:
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+        self.walls = {'top': True, 'right': True, 'bottom': True, 'left': True}
+        self.visited = False
+        self.thickness = 4
 
-    # traverse every node via breadth first search
-    current = next(iter(grid))
-    visited = {key: False for key in grid}
-    stack = [current]
-    visited[current] = True
-    path = []
-    while len(stack) > 0:
-        path.append(current)
-        unvisited = [n for _, n in grid[current] if not visited[n]]
-        if len(unvisited) == 0:
-            current = stack.pop()
-        else:
-            stack.append(current)
-            # randomly choose the next node to add variety to the maze
-            current = random.choice(unvisited)
-            visited[current] = True
+    def draw(self, sc):
+        x, y = self.x * TILE, self.y * TILE
 
-    # remove the unused exits into other nodes (like placing walls between nodes)
-    graph = {key: [] for key in grid}
-    for i in range(len(path)-1):
-        graph[path[i]].append((random.randint(0, MAX_WEIGHT), path[i+1]))
-    
-    return graph
+        if self.walls['top']:
+            pygame.draw.line(sc, pygame.Color('darkorange'), (x, y), (x + TILE, y), self.thickness)
+        if self.walls['right']:
+            pygame.draw.line(sc, pygame.Color('darkorange'), (x + TILE, y), (x + TILE, y + TILE), self.thickness)
+        if self.walls['bottom']:
+            pygame.draw.line(sc, pygame.Color('darkorange'), (x + TILE, y + TILE), (x , y + TILE), self.thickness)
+        if self.walls['left']:
+            pygame.draw.line(sc, pygame.Color('darkorange'), (x, y + TILE), (x, y), self.thickness)
 
-maze = load_maze(10, 10)
-ax, ay, _ = tilt.get_tilt_data()
+    def get_rects(self):
+        rects = []
+        x, y = self.x * TILE, self.y * TILE
+        if self.walls['top']:
+            rects.append(pygame.Rect( (x, y), (TILE, self.thickness) ))
+        if self.walls['right']:
+            rects.append(pygame.Rect( (x + TILE, y), (self.thickness, TILE) ))
+        if self.walls['bottom']:
+            rects.append(pygame.Rect( (x, y + TILE), (TILE , self.thickness) ))
+        if self.walls['left']:
+            rects.append(pygame.Rect( (x, y), (self.thickness, TILE) ))
+        return rects
 
+    def check_cell(self, x, y):
+        find_index = lambda x, y: x + y * cols
+        if x < 0 or x > cols - 1 or y < 0 or y > rows - 1:
+            return False
+        return self.grid_cells[find_index(x, y)]
+
+    def check_neighbors(self, grid_cells):
+        self.grid_cells = grid_cells
+        neighbors = []
+        top = self.check_cell(self.x, self.y - 1)
+        right = self.check_cell(self.x + 1, self.y)
+        bottom = self.check_cell(self.x, self.y + 1)
+        left = self.check_cell(self.x - 1, self.y)
+        if top and not top.visited:
+            neighbors.append(top)
+        if right and not right.visited:
+            neighbors.append(right)
+        if bottom and not bottom.visited:
+            neighbors.append(bottom)
+        if left and not left.visited:
+            neighbors.append(left)
+        return choice(neighbors) if neighbors else False
+
+
+def remove_walls(current, next):
+    dx = current.x - next.x
+    if dx == 1:
+        current.walls['left'] = False
+        next.walls['right'] = False
+    elif dx == -1:
+        current.walls['right'] = False
+        next.walls['left'] = False
+    dy = current.y - next.y
+    if dy == 1:
+        current.walls['top'] = False
+        next.walls['bottom'] = False
+    elif dy == -1:
+        current.walls['bottom'] = False
+        next.walls['top'] = False
+
+def generate_maze():
+    grid_cells = [Cell(col, row) for row in range(rows) for col in range(cols)]
+    current_cell = grid_cells[0]
+    array = []
+    break_count = 1
+
+    while break_count != len(grid_cells):
+        current_cell.visited = True
+        next_cell = current_cell.check_neighbors(grid_cells)
+        if next_cell:
+            next_cell.visited = True
+            break_count += 1
+            array.append(current_cell)
+            remove_walls(current_cell, next_cell)
+            current_cell = next_cell
+        elif array:
+            current_cell = array.pop()
+    return grid_cells
